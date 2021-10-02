377304862004bc4b92ca097334966265a89b9b1e
Vonne Harvey Tuso <vonne.tuso@usjr.edu.ph>
2021-10-02 19:11:35 +0800

    simple angular calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,67 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-calculator';
+  finalAns = ''
+  answer = 0;
+  fNum = 0;
+  sNum = 0;
+  fNAN = false;
+  sNAN = false;
+
+  getFInput(num: string)
+  {
+    let value = parseInt(num);
+    this.finalAns = '';
+
+    if(!isNaN(value))
+    {
+      this.fNum = value;
+      this.fNAN = false;
+    }
+    else
+      this.fNAN = true;
+  }
+
+  getSInput(num: string)
+  {
+    let value = parseInt(num);
+    this.finalAns = '';
+
+    if(!isNaN(value))
+    {
+      this.sNum = value;
+      this.sNAN = false;
+    }
+    else
+      this.sNAN = true;
+  }
+
+  toCalculate(choice: string)
+  {
+    let op = parseInt(choice);
+
+    if(this.fNAN || this.sNAN)
+      this.finalAns = "Invalid Input";    
+    else
+    {
+      switch(op)
+      {
+        case 1: this.answer = this.fNum + this.sNum; break;
+        case 2: this.answer = this.fNum - this.sNum; break;
+        case 3: this.answer = this.fNum * this.sNum; break;
+        case 4: this.answer = this.fNum / this.sNum; break;
+      }
+      this.finalAns = this.answer.toString();
+
+      if(op === 4 && (this.sNum === 0 && this.fNum !== 0))
+        this.finalAns = "Divisor can not be zero!";
+    }
+
+    this.onClick();
+  }
+
+  onClick()
+  {
+    console.log(this.fNum + "and" + this.sNum);
+  }
 }
